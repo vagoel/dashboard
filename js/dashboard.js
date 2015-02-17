031931569082e374afc64b076c9165e58b5b3dbf
Varun Goel <vagoel@adobe.com>
2015-02-17 19:22:02 +0530

    widgets are moveable now..

--- js/dashboard.js
+++ js/dashboard.js
@@ -127,3 +127,22 @@ var data = {
     ]
 }
 }
+
+
+jQuery(function($) {
+    var panelList = $('#draggablePanelList');
+
+    panelList.sortable({
+        // Only make the .panel-heading child elements support dragging.
+        // Omit this to make then entire <li>...</li> draggable.
+        handle: '.panel-heading', 
+        update: function() {
+            /*$('.panel', panelList).each(function(index, elem) {
+                 var $listItem = $(elem),
+                     newIndex = $listItem.index();
+
+                 // Persist the new indices.
+            });*/
+        }
+    });
+});
